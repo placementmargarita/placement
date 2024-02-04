f95de196d4378e9b1b7c41408695e947c6e55123
placementmargarita <158606430+placementmargarita@users.noreply.github.com>
2024-02-04 10:32:57 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,14 +2,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
-from myapp.views import *
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     #link urls from an app
-    path("", myapp.views.indexfr, name="indexfr"),
+    
     path("myapp/", include("myapp.urls")),
     path("gestion/", include("gestion.urls")),
 ]
